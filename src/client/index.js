023824153401573d973c2d99707333fc999974aa
Nadim Rai <nadim_rai@yahoo.com>
2023-09-29 00:38:54 +0800

    installed html-webpack-plugin and set up sass with webpack

---
+++ src/client/index.js
@@ -0,0 +1,4 @@
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
-const path = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     entry: './src/client/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
 }
