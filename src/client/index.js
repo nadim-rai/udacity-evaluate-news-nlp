4619b46f249a5c6ab4cfe7424558cefd82bce6f3
Nadim Rai <nadim_rai@yahoo.com>
2023-09-29 01:06:05 +0800

    js folder files are set to refer through distribution folder main js file

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,10 @@
+import { handleSubmit } from './js/formHandler'
+
 import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+
+export{
+    handleSubmit
+}
